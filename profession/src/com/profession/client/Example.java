c23d6516c61a56538af7632c6edd2c5ee3c24716
IgalYadid <IgalYadid@IgalYadid-PC>
2012-10-21 16:07:28 +0200

    first commit

---
+++ profession/src/com/profession/client/Example.java
@@ -0,0 +1,12 @@
+package com.profession.client;
+
+import com.google.gwt.user.client.ui.Composite;
+
+
+public abstract class Example extends Composite {
+
+    public abstract String getMethod () ;
+    
+    public abstract String getSimpleName ();
+    
+}
---
+++ profession/src/com/profession/client/FqlQueryExample.java
@@ -0,0 +1,54 @@
+package com.profession.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+/**
+ * Fql Query Example
+ * 
+ * @author olamariussagli
+ */
+public class FqlQueryExample extends Example {
+
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private SimplePanel result = new SimplePanel();
+
+	public FqlQueryExample(FBCore fbCore) {
+
+		String method = "fql.query";
+		String fql = "SELECT uid, first_name, last_name FROM user WHERE uid = 744450545";
+
+		JSONObject query = new JSONObject();
+		query.put("method", new JSONString(method));
+		query.put("query", new JSONString(fql));
+		mainPanel.add(new HTML(fql));
+		mainPanel.add(result);
+		result.setWidget(new HTML("Getting result..."));
+
+		// Execute query
+		fbCore.api(query.getJavaScriptObject(),
+				new Callback<JavaScriptObject>() {
+					public void onSuccess(JavaScriptObject response) {
+						result.setWidget(new HTML(new JSONObject(response)
+								.toString()));
+					}
+				});
+
+		initWidget(mainPanel);
+	}
+
+	@Override
+	public String getMethod() {
+		return "fql.query";
+	}
+
+	@Override
+	public String getSimpleName() {
+		return "FqlQueryExample";
+	}
+}
