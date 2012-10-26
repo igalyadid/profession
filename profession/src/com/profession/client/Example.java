df90553d49dd73e297afbeb5d72b816bbe195cfd
IgalYadid <IgalYadid@IgalYadid-PC>
2012-10-26 09:47:27 +0200

    test

--- profession/src/com/profession/client/Example.java
+++ profession/src/com/profession/client/Example.java
@@ -2,7 +2,7 @@ package com.profession.client;
 
 import com.google.gwt.user.client.ui.Composite;
 
-
+//igal
 public abstract class Example extends Composite {
 
     public abstract String getMethod () ;
